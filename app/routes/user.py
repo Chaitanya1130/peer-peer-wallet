f80f453dd362afc7b5999ad720c71b7840a66dab
Chaitanya1130 <svedula4@gitam.in>
2025-05-05 10:15:34 +0530

    added pytest to github

--- app/routes/user.py
+++ app/routes/user.py
@@ -1,10 +1,14 @@
 from fastapi import APIRouter, Depends, HTTPException,Query
 from bson import ObjectId
+from pydantic import BaseModel
 # routes/user.py
 from app.models.wallet import Wallet, WalletCreate, WalletInDB
 from app.models.user import User, UserCreate, UserInDB
 from app.database import user_collection, wallet_collection
+from app.models.transaction import Transaction, TransactionCreate ,TransactioninDB
+from app.database import transaction_collection
 from app.database import getUserID, getuserByusername, getuserByemail, getBalanceByusername
+from langchain_ollama import OllamaLLM
 # from app.utils import hash_password
 router = APIRouter()
 def HashPassword(password:str):
@@ -93,3 +97,34 @@ async def retriveDetails(username: str = Query(..., description="Username of the
         hash_pass=Curruser["hash_pass"],
         balance=balance
     )
+
+llm=OllamaLLM(model="llama2", temperature=0.9)
+@router.post("/giveMEasuitablepassword")
+async def ollamaGivespass(username:str):
+     prompt = f"Generate a random example string with {len(username)} characters, combining letters, numbers, and symbols. Only output the string, no explanation."
+     try:
+          response=llm.invoke(prompt)
+          gen_pass=response.strip()
+          return {"username":username,"generaatedpassword":gen_pass}
+     except Exception as e:
+          raise HTTPException(status_code=500, detail=f"Error generating password: {e}")
+
+class BudgetAdviceRequest(BaseModel):
+    username: str
+    time: int
+    
+@router.post("/budgetadvice")
+async def budgetAdvice(request:BudgetAdviceRequest):
+     user=request.username
+     time=request.time
+     transactions=await transaction_collection.find({"sender_username":user}).to_list(length=time)
+     if not transactions:
+          raise HTTPException(status_code=404, detail="No transactions found for this user")
+     else:
+          prompt = f"Based on the transactions for {user} in the last {time}, make a ordered list of the amount he has sent in the transaction. For example 1.1000/- 2.10000/-."
+          try:
+               response=llm.invoke(prompt)
+               advice=response.strip()
+               return {"username":user,"advice":advice,"time":time}
+          except Exception as e:
+               raise HTTPException(status_code=500, detail=f"Error generating budget advice: {e}")
