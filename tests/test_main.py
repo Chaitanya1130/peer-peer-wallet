0604c097f9a343b131b3b157ab6ce06d43caca58
Chaitanya1130 <svedula4@gitam.in>
2025-05-05 10:44:48 +0530

    changed test/test 3

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,7 +3,7 @@ import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from fastapi.testclient import TestClient
-from wallet import app
+from app.main import app
 
 client = TestClient(app)
 
