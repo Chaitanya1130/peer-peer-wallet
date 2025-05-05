eb68820be79bef130c9067f657f8fc98fd925d6f
Chaitanya1130 <svedula4@gitam.in>
2025-05-05 10:37:08 +0530

    changed test/test

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,11 +1,15 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from fastapi.testclient import TestClient
 from wallet import app
 
+client = TestClient(app)
 
-client=TestClient(app)
 def test_user_signup():
-    response = client.post("/user/signup",json={"username":"testuser","email":"testsemail","password":"testpass"})
-    data=response.json()
+    response = client.post("/user/signup", json={"username": "testuser", "email": "testsemail"})
+    data = response.json()
     assert response.status_code == 200 or response.status_code == 400
     assert "username" in data
     assert data["username"] == "testuser"
